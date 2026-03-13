56972d187e26f34b0133704dd40e5587fe3e86c1
vahagn.danielyan <vahagn.danielyan2004@gmail.com>
2026-03-13 17:19:58 +0400

    UserService: Refactor SetupServicesExtensions, add automigration, change db connection name.

--- Services/UserService/UserService.API/Extensions/SetupServicesExtensions.cs
+++ Services/UserService/UserService.API/Extensions/SetupServicesExtensions.cs
@@ -1,12 +1,14 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using UserService.API.Configurations;
 using UserService.Application;
 using UserService.Infrastructure;
 using UserService.Persistence;
+using UserService.Persistence.Database;
 
 namespace UserService.API.Extensions;
 
-public static class SetupServicesExtensions
+public static class SetupApplicationExtensions
 {
     public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
     {
@@ -23,23 +25,40 @@ public static class SetupServicesExtensions
     {
         if (app.Environment.IsDevelopment())
         {
-            var swaggerSettings = app.Services
-                .GetRequiredService<IOptions<SwaggerSettings>>()
-                .Value;
-            
-            if (swaggerSettings.IsEnabled)
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint(swaggerSettings.Endpoint, swaggerSettings.Title);
-                    c.RoutePrefix = swaggerSettings.RoutePrefix;
-                });
-            }
+            app.UseSwaggerIfEnabled();
+            app.ApplyAutoMigrationIfEnabled();
         }
 
         app.MapControllers();
 
         return app;
+    }
+
+    private static void UseSwaggerIfEnabled(this WebApplication app)
+    {
+        var swaggerSettings = app.Services
+            .GetRequiredService<IOptions<SwaggerSettings>>()
+            .Value;
+
+        if (!swaggerSettings.IsEnabled) return;
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint(swaggerSettings.Endpoint, swaggerSettings.Title);
+            c.RoutePrefix = swaggerSettings.RoutePrefix;
+        });
+    }
+
+    private static void ApplyAutoMigrationIfEnabled(this WebApplication app)
+    {
+        var isAutoMigrationEnabled = app.Configuration.GetValue<bool>("IsAutoMigrationEnabled");
+
+        if (!isAutoMigrationEnabled) return;
+
+        using var scope = app.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<BpUserServiceDbContext>();
+
+        db.Database.Migrate();
     }
 }

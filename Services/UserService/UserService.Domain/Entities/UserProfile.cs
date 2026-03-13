2443ca3b8b95872fad1c46c9ce0887f198accc75
vahagn.danielyan <vahagn.danielyan2004@gmail.com>
2026-03-13 17:18:52 +0400

    UserService: Add phone in UserProfile

--- Services/UserService/UserService.Domain/Entities/UserProfile.cs
+++ Services/UserService/UserService.Domain/Entities/UserProfile.cs
@@ -4,22 +4,20 @@ namespace UserService.Domain.Entities;
 
 public sealed class UserProfile : AggregateRoot<Guid>
 {
-    private UserProfile(Guid identityUserId, FirstName firstName, LastName lastName, Phone phone)
+    private UserProfile(Guid identityUserId, FirstName firstName, LastName lastName)
     {
         Id = Guid.NewGuid();
         IdentityUserId = identityUserId;
         FirstName = firstName;
         LastName = lastName;
-        Phone = phone;
         CreatedAt = DateTime.UtcNow;
     }
 
     public Guid IdentityUserId { get; private set; }
     public FirstName FirstName { get; private set; }
     public LastName LastName { get; private set; }
-    public Phone Phone { get; private set; }
     public DateTime CreatedAt { get; private set; }
 
-    public static UserProfile Create(Guid identityUserId, FirstName firstName, LastName lastName, Phone phone) =>
-        new(identityUserId, firstName, lastName, phone);
+    public static UserProfile Create(Guid identityUserId, FirstName firstName, LastName lastName) =>
+        new(identityUserId, firstName, lastName);
 }
